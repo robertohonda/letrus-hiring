cf46da638d2ae324c5b6d8caaa4f92f34141fe0a
Roberto Honda <roberto_junior_honda@hotmail.com>
2019-07-11 22:48:12 -0400

    init makeTextWithMistakes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,74 @@
 import React, { Component } from 'react'
 import './App.css'
 
-export function makeTextWithMistakes(text, mistakes) {
-  // ...
+export const sortMistakes = (mistakes) => {
+  const mistakesCopy = [...mistakes]
+  return mistakesCopy.sort(
+    (mistake, nextMistake) =>
+      mistake.paragraph - nextMistake.paragraph ||
+      mistake.start - nextMistake.start,
+  )
 }
+
+export const makeTextWithMistakes = (text, mistakes) => {
+  const paragraphs = text.split('\n')
+  const sortedMistakes = sortMistakes(mistakes)
+  const result = paragraphs.map((paragraph) => [paragraph])
+
+  sortedMistakes.forEach((mistake) => {
+    const { start, end, paragraph } = mistake
+    const resultParagraph = result[paragraph]
+    const selectedParagraph = paragraphs[paragraph]
+
+    if (resultParagraph.length === 1)
+      resultParagraph[0] = selectedParagraph.slice(0, start)
+    else {
+      const lastIndex = resultParagraph.length - 1
+      // console.log(mistake, resultParagraph)
+      // console.log('aaaaa', JSON.stringify(selectedParagraph))
+      console.log(mistake, resultParagraph[lastIndex])
+      resultParagraph.push(
+        selectedParagraph.slice(resultParagraph[lastIndex].length, start),
+      )
+    }
+
+    resultParagraph.push(<em>{selectedParagraph.slice(start, end)}</em>)
+  })
+
+  return result
+}
+
+// console.log(
+//   sortMistakes([
+//     {
+//       start: 15,
+//       end: 20,
+//       paragraph: 0,
+//     },
+//     {
+//       start: 8,
+//       end: 16,
+//       paragraph: 1,
+//     },
+//   ]),
+// )
 
 class App extends Component {
   renderParagraph = (paragraph, i) => {
     return <p key={i}>{paragraph}</p>
   }
-
+  // ...
   render() {
     const text = 'Meu texto está erado\nSegundo palagrafo'
     const mistakes = [
       {
         start: 15,
         end: 20,
+        paragraph: 0,
+      },
+      {
+        start: 0,
+        end: 5,
         paragraph: 0,
       },
       {
@@ -33,10 +86,14 @@ class App extends Component {
     // achar necessário
     // ========================================================================
 
-    const textWithMistakes = [
-      ['Meu texto está ', <em>erado</em>],
-      ['Segundo ', <em>palagrafo</em>],
-    ]
+    // const textWithMistakes = [
+    //   ['Meu texto está ', <em>erado</em>],
+    //   ['Segundo ', <em>palagrafo</em>],
+    // ]
+
+    const textWithMistakes = makeTextWithMistakes(text, mistakes)
+
+    // console.log(textWithMistakes)
 
     return (
       <div className="App">{textWithMistakes.map(this.renderParagraph)}</div>
